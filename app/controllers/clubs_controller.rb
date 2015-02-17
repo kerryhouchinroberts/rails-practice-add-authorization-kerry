fc34c8ea2e0e609bd36fbc9011af1943debf97f6
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-17 13:30:33 -0700

    Block access to club show page for those who are not members of said club

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -8,6 +8,12 @@ class ClubsController < ApplicationController
 
   def show
     @club = Club.find(params[:id])
+    @student = current_student.clubs
+
+    unless @student.include? @club
+      render 'public/403.html'
+    end
+
   end
 
   private
